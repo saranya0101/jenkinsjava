0265a1f5d38cf7f15c805330a4fe8355ef83a3ea
saranya0101 <saranyapolamraju@gmail.com>
2024-02-04 15:51:45 +0530

    changes made

--- App1.java
+++ App1.java
@@ -1,6 +1,8 @@
 public class App1{
     public static void main(String[] args){
         System.out.println("Java App Pulled & Build by Jenkins");
+        System.out.println("Sara");
+        System.out.println("Chikki");
 
     }
 
